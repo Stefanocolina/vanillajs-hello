5aca14dfbeac02141019af3a4784927ff0281c4f
Stefanocolina <colinastefano4@gmail.com>
2024-07-22 21:58:30 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  console.log("¡Hola Rigo desde la consola! ");
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generateExcuse = () => {
+  let pronoun = ["A", "The"];
+  let subject = ["jogger", "racoon", "dog", "driver", "comedian", "pincone"];
+  let action = ["took my", "threw my", "yelled at my", "stole my", "bit my"];
+  let possession = ["homework", "toe", "car", "shoe"];
+  let where = ["on the street", "in my house", "in my driveway"];
+
+  let proIndx = Math.floor(Math.random() * pronoun.length);
+  let subjIndx = Math.floor(Math.random() * subject.length);
+  let actIndx = Math.floor(Math.random() * action.length);
+  let posIndx = Math.floor(Math.random() * possession.length);
+  let wheIndx = Math.floor(Math.random() * where.length);
+
+  return `${pronoun[proIndx]} ${subject[subjIndx]} ${action[actIndx]} ${possession[posIndx]} ${where[wheIndx]}`;
 };
